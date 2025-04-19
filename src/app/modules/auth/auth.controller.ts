14fe87fb0628f0e3136590283438c567742d228e
safimiazi <mohibullamiazi@gmail.com>
2025-04-19 12:15:25 +0600

    feat: enhance user authentication by adding password comparison and improving error handling in registration and login

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,12 +1,13 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import status from "http-status";
 import { usersModel } from "../users/users.model";
 import { Iusers } from "../users/users.interface";
 import AppError from "../../errors/AppError";
+import comparePassword from "../../utils/comparePassword";
 
-const Register = catchAsync(async (req: Request, res: Response) => {
+const Register = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   // check if user already exists:
   const user: Iusers | null = await usersModel.findOne({
     email: req.body.email,
@@ -15,18 +16,33 @@ const Register = catchAsync(async (req: Request, res: Response) => {
     throw new AppError(status.CONFLICT, "User already exists");
   }
 
-
   // create user:
-  // const newUser = await usersModel.create(req.body);
-console.log(req.body);
+  const newUser = await usersModel.create(req.body);
   sendResponse(res, {
     statusCode: status.CREATED,
     success: true,
     message: "Register successfully",
-    data: "newUser",
+    data: newUser,
   });
 });
-const Login = catchAsync(async (req: Request, res: Response) => {
+
+
+const Login = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    // check if  user not exists:
+    const user: Iusers | null = await usersModel.findOne({
+      email: req.body.email,
+    });
+    if (!user) {
+      throw new AppError(status.CONFLICT, "User not exists! please register");
+    }
+
+    // compare password:
+    const isMatch = await comparePassword(req.body.password, user.password);
+
+    
+    if (!isMatch) {
+      throw new AppError(status.UNAUTHORIZED, "Password is incorrect");
+    }
   const result = "";
   sendResponse(res, {
     statusCode: status.CREATED,
--- src/app/utils/catchAsync.ts
+++ src/app/utils/catchAsync.ts
@@ -4,7 +4,6 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
 const catchAsync = (fn: RequestHandler) => {
     return (req : Request, res: Response, next: NextFunction) => {
         Promise.resolve(fn(req, res, next)).catch((error) => {
-            console.log("Error in catchAsync:", error);
             next(error);
         })
     }
