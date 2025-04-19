10795f861335f8b08f999680f45000f08d71fdc8
safimiazi <mohibullamiazi@gmail.com>
2025-04-19 18:03:21 +0600

    feat: enhance brand routes with authentication and authorization middleware

--- src/app/modules/brand/brand.routes.ts
+++ src/app/modules/brand/brand.routes.ts
@@ -1,15 +1,26 @@
+import express from "express";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { brandController } from "./brand.controller";
+import { brandPostValidation, brandUpdateValidation } from "./brand.validation";
+import { authenticate, authorize } from "../../middlewares/authGuard";
+import { ROLE } from "../../constant/role";
 
-    import express from "express";
-    import { validateRequest } from "../../middlewares/validateRequest";
-    import { brandController } from "./brand.controller";
-    import { brandPostValidation,brandUpdateValidation } from "./brand.validation";
+const router = express.Router();
 
-    const router = express.Router();
-    
-    router.post("/post_brand", validateRequest(brandPostValidation), brandController.postBrand);
-    router.get("/get_all_brand", brandController.getAllBrand);
-    router.get("/get_single_brand/:id", brandController.getSingleBrand);
-    router.put("/update_brand/:id", validateRequest(brandUpdateValidation), brandController.updateBrand);
-    router.delete("/delete_brand/:id", brandController.deleteBrand);
-    
-    export const brandRoutes = router;
+router.post(
+  "/post_brand",
+  validateRequest(brandPostValidation),
+  authenticate,
+  authorize(ROLE.ADMIN, ROLE.VENDOR),
+  brandController.postBrand
+);
+router.get("/get_all_brand", brandController.getAllBrand);
+router.get("/get_single_brand/:id", brandController.getSingleBrand);
+router.put(
+  "/update_brand/:id",
+  validateRequest(brandUpdateValidation),
+  brandController.updateBrand
+);
+router.delete("/delete_brand/:id", brandController.deleteBrand);
+
+export const brandRoutes = router;
