bbc0f676cd5fa0055001ef102501f22ba153200b
safimiazi <mohibullamiazi@gmail.com>
2025-04-20 16:49:00 +0600

    feat: enhance attribute controller and service with improved error handling and validation

--- src/app/modules/attribute/attribute.service.ts
+++ src/app/modules/attribute/attribute.service.ts
@@ -1,110 +1,128 @@
 import { attributeModel } from "./attribute.model";
-      import { ATTRIBUTE_SEARCHABLE_FIELDS } from "./attribute.constant";
-    import QueryBuilder from "../../builder/QueryBuilder";
-    import status from "http-status";
-    import AppError from "../../errors/AppError";
-    
+import { ATTRIBUTE_SEARCHABLE_FIELDS } from "./attribute.constant";
+import QueryBuilder from "../../builder/QueryBuilder";
+import status from "http-status";
+import AppError from "../../errors/AppError";
 
-
-
-
-    export const attributeService = {
-      async postAttributeIntoDB(data: any) {
-      try {
-        return await attributeModel.create(data);
-         } catch (error: unknown) {
-          if (error instanceof Error) {
-            throw new Error(`${error.message}`);
-          } else {
-            throw new Error("An unknown error occurred while fetching by ID.");
-          }
-        }
-      },
-      async getAllAttributeFromDB(query: any) {
-      try {
-    
-    
+export const attributeService = {
+  async postAttributeIntoDB(data: any) {
+    try {
+      const isExist = await attributeModel.findOne({ name: data.name });
+      if (isExist) {
+        throw new AppError(status.CONFLICT, "attribute already exists");
+      }
+      return await attributeModel.create(data);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
+  async getAllAttributeFromDB(query: any) {
+    try {
       const service_query = new QueryBuilder(attributeModel.find(), query)
-            .search(ATTRIBUTE_SEARCHABLE_FIELDS)
-            .filter()
-            .sort()
-            .paginate()
-            .fields();
-      
-          const result = await service_query.modelQuery;
-          const meta = await service_query.countTotal();
-          return {
-            result,
-            meta,
-          };
-    
-         } catch (error: unknown) {
-          if (error instanceof Error) {
-            throw new Error(`${error.message}`);
-          } else {
-            throw new Error("An unknown error occurred while fetching by ID.");
-          }
-        }
-      },
-      async getSingleAttributeFromDB(id: string) {
-        try {
-        return await attributeModel.findById(id);
-         } catch (error: unknown) {
-          if (error instanceof Error) {
-            throw new Error(`${error.message}`);
-          } else {
-            throw new Error("An unknown error occurred while fetching by ID.");
-          }
-        }
-      },
-      async updateAttributeIntoDB(data: any) {
-      try {
-    
-    
-    
-      const isDeleted = await attributeModel.findOne({ _id: data.id });
-        if (isDeleted?.isDelete) {
-          throw new AppError(status.NOT_FOUND, "attribute is already deleted");
-        }
-    
-        const result = await attributeModel.updateOne({ _id: data.id }, data, {
-          new: true,
-        });
-        if (!result) {
-          throw new Error("attribute not found.");
-        }
-        return result;
-    
-    
-         } catch (error: unknown) {
-          if (error instanceof Error) {
-            throw new Error(`${error.message}`);
-          } else {
-            throw new Error("An unknown error occurred while fetching by ID.");
-          }
-        }
-      },
-      async deleteAttributeFromDB(id: string) {
-        try {
-    
-    
-     // Step 1: Check if the attribute exists in the database
-        const isExist = await attributeModel.findOne({ _id: id });
-    
-        if (!isExist) {
-          throw new AppError(status.NOT_FOUND, "attribute not found");
-        }
-    
-        // Step 4: Delete the home attribute from the database
-        await attributeModel.updateOne({ _id: id }, { isDelete: true });
-        return;
-    
-         } catch (error: unknown) {
-          if (error instanceof Error) {
-            throw new Error(`${error.message}`);
-          } else {
-            throw new Error("An unknown error occurred while fetching by ID.");
-          }
-        }
-      },
-    };
+        .search(ATTRIBUTE_SEARCHABLE_FIELDS)
+        .filter()
+        .sort()
+        .paginate()
+        .fields();
+
+      const result = await service_query.modelQuery.populate("attributeOption");
+      const meta = await service_query.countTotal();
+      return {
+        result,
+        meta,
+      };
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
+  async getSingleAttributeFromDB(id: string) {
+    try {
+      const attribute = await attributeModel.findOne({ _id: id });
+      if (attribute?.isDelete) {
+        throw new AppError(status.NOT_FOUND, "attribute is already deleted");
+      }
+      if (!attribute?.isActive) {
+        throw new AppError(status.NOT_FOUND, "attribute is not active");
+      }
+
+      const result = await attributeModel
+        .findById(id)
+        .populate("attributeOption");
+      if (!result) {
+        throw new AppError(status.NOT_FOUND, "attribute not found");
+      }
+
+      return result;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
+  async updateAttributeIntoDB(data: any, id: string) {
+    try {
+      const attribute = await attributeModel.findOne({ _id: id });
+
+      if (!attribute) {
+        throw new AppError(status.NOT_FOUND, "attribute not found");
+      }
+      if (attribute?.isDelete) {
+        throw new AppError(status.NOT_FOUND, "attribute is already deleted");
+      }
+      if (!attribute?.isActive) {
+        throw new AppError(status.NOT_FOUND, "attribute is not active");
+      }
+
+      const result = await attributeModel.updateOne({ _id: id }, data, {
+        new: true,
+      });
+   
+      return result;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
+  async deleteAttributeFromDB(id: string) {
+    try {
+      // Step 1: Check if the attribute exists in the database
+      const isExist = await attributeModel.findOne({ _id: id });
+      if (!isExist) {
+        throw new AppError(status.NOT_FOUND, "attribute not found");
+      }
+      // Step 2: Check if the attribute is already deleted
+      if (isExist.isDelete) {
+        throw new AppError(status.NOT_FOUND, "attribute already deleted");
+      }
+      // Step 3: Check if the attribute is active
+      if (!isExist.isActive) {
+        throw new AppError(status.NOT_FOUND, "attribute already inactive");
+      }
+
+   
+
+      // Step 4: Delete the home attribute from the database
+      await attributeModel.updateOne({ _id: id }, { isDelete: true });
+      return;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
+};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -9,6 +9,9 @@ import { adminRoutes } from "../modules/admin/admin.routes";
 import { managerRoutes } from "../modules/manager/manager.routes";
 import { customerRoutes } from "../modules/customer/customer.routes";
 import { brandRoutes } from "../modules/brand/brand.routes";
+import path from "path";
+import { attributeRoutes } from "../modules/attribute/attribute.routes";
+import { attributeOptionRoutes } from "../modules/attributeOption/attributeOption.routes";
 
 const router = Router();
 
@@ -36,6 +39,14 @@ const moduleRoutes = [
     {
         path: "/brands",
         route: brandRoutes
+    },
+    {
+        path: "atributes",
+        route: attributeRoutes
+    },
+    {
+        path: "atributeOptions",
+        route: attributeOptionRoutes
     },
     {
         path: "/products",
