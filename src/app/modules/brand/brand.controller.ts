1e1853142d585085fc133d4de114412dbfd5b2dc
safimiazi <mohibullamiazi@gmail.com>
2025-04-20 11:21:10 +0600

    feat: enhance brand update logic with file existence checks and deletion handling for brand images

--- src/app/modules/brand/brand.controller.ts
+++ src/app/modules/brand/brand.controller.ts
@@ -4,6 +4,8 @@ import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import status from "http-status";
 import { brandModel } from "./brand.model";
+import path from "path";
+import fs from "fs";
 
 const postBrand = catchAsync(async (req: Request, res: Response) => {
   const result = await brandService.postBrandIntoDB(req.body);
@@ -36,19 +38,38 @@ const getSingleBrand = catchAsync(async (req: Request, res: Response) => {
 });
 
 const updateBrand = catchAsync(async (req: Request, res: Response) => {
-  const {id} = req.params;
-  const brand   = await brandModel.findById(id);
+  const { id } = req.params;
+  const brand = await brandModel.findById(id);
   if (!brand) {
     throw new Error("Brand not found");
   }
   if (brand.isDelete) {
-   throw new Error("Brand is already deleted");
+    throw new Error("Brand is already deleted");
   }
-  if(!brand.isActive) {
+  if (!brand.isActive) {
     throw new Error("Brand is not active");
   }
-  
 
+  if (req.body.brandImage) {
+    const existingBrandImagePath = path.join(
+      __dirname,
+      "../../..",
+      brand.brandImage as string
+    );
+    console.log(existingBrandImagePath);
+    if (fs.existsSync(existingBrandImagePath)) {
+      if (brand.brandImage && brand.brandImage !== req.body.brandImage) {
+        fs.unlink(existingBrandImagePath, (err: any) => {
+          if (err) {
+            throw new Error("Error deleting file");
+          }
+        });
+      }
+      req.body.brandImage = req.body.brandImage.replace(/\\/g, "/");
+    } else {
+      throw new Error("File does not exist");
+    }
+  }
 
   const result = await brandService.updateBrandIntoDB(req.body);
   sendResponse(res, {
