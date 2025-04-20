6eb22a0b05f35dbf699abc7df6d300473cace1bc
safimiazi <mohibullamiazi@gmail.com>
2025-04-20 10:05:38 +0600

    feat: refactor brand service methods for improved error handling and code clarity

--- src/app/modules/brand/brand.service.ts
+++ src/app/modules/brand/brand.service.ts
@@ -1,110 +1,95 @@
 import { brandModel } from "./brand.model";
-      import { BRAND_SEARCHABLE_FIELDS } from "./brand.constant";
-    import QueryBuilder from "../../builder/QueryBuilder";
-    import status from "http-status";
-    import AppError from "../../errors/AppError";
-    
+import { BRAND_SEARCHABLE_FIELDS } from "./brand.constant";
+import QueryBuilder from "../../builder/QueryBuilder";
+import status from "http-status";
+import AppError from "../../errors/AppError";
 
-
-
-
-    export const brandService = {
-      async postBrandIntoDB(data: any) {
-      try {
-        return await brandModel.create(data);
-         } catch (error: unknown) {
-          if (error instanceof Error) {
-            throw new Error(`${error.message}`);
-          } else {
-            throw new Error("An unknown error occurred while fetching by ID.");
-          }
-        }
-      },
-      async getAllBrandFromDB(query: any) {
-      try {
-    
-    
+export const brandService = {
+  async postBrandIntoDB(data: any) {
+    try {
+      return await brandModel.create(data);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
+  async getAllBrandFromDB(query: any) {
+    try {
       const service_query = new QueryBuilder(brandModel.find(), query)
-            .search(BRAND_SEARCHABLE_FIELDS)
-            .filter()
-            .sort()
-            .paginate()
-            .fields();
-      
-          const result = await service_query.modelQuery;
-          const meta = await service_query.countTotal();
-          return {
-            result,
-            meta,
-          };
-    
-         } catch (error: unknown) {
-          if (error instanceof Error) {
-            throw new Error(`${error.message}`);
-          } else {
-            throw new Error("An unknown error occurred while fetching by ID.");
-          }
-        }
-      },
-      async getSingleBrandFromDB(id: string) {
-        try {
-        return await brandModel.findById(id);
-         } catch (error: unknown) {
-          if (error instanceof Error) {
-            throw new Error(`${error.message}`);
-          } else {
-            throw new Error("An unknown error occurred while fetching by ID.");
-          }
-        }
-      },
-      async updateBrandIntoDB(data: any) {
-      try {
-    
-    
-    
+        .search(BRAND_SEARCHABLE_FIELDS)
+        .filter()
+        .sort()
+        .paginate()
+        .fields();
+
+      const result = await service_query.modelQuery;
+      const meta = await service_query.countTotal();
+      return {
+        result,
+        meta,
+      };
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
+  async getSingleBrandFromDB(id: string) {
+    try {
+      return await brandModel.findById(id);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
+  async updateBrandIntoDB(data: any) {
+    try {
       const isDeleted = await brandModel.findOne({ _id: data.id });
-        if (isDeleted?.isDelete) {
-          throw new AppError(status.NOT_FOUND, "brand is already deleted");
-        }
-    
-        const result = await brandModel.updateOne({ _id: data.id }, data, {
-          new: true,
-        });
-        if (!result) {
-          throw new Error("brand not found.");
-        }
-        return result;
-    
-    
-         } catch (error: unknown) {
-          if (error instanceof Error) {
-            throw new Error(`${error.message}`);
-          } else {
-            throw new Error("An unknown error occurred while fetching by ID.");
-          }
-        }
-      },
-      async deleteBrandFromDB(id: string) {
-        try {
-    
-    
-     // Step 1: Check if the brand exists in the database
-        const isExist = await brandModel.findOne({ _id: id });
-    
-        if (!isExist) {
-          throw new AppError(status.NOT_FOUND, "brand not found");
-        }
-    
-        // Step 4: Delete the home brand from the database
-        await brandModel.updateOne({ _id: id }, { isDelete: true });
-        return;
-    
-         } catch (error: unknown) {
-          if (error instanceof Error) {
-            throw new Error(`${error.message}`);
-          } else {
-            throw new Error("An unknown error occurred while fetching by ID.");
-          }
-        }
-      },
-    };
+      if (isDeleted?.isDelete) {
+        throw new AppError(status.NOT_FOUND, "brand is already deleted");
+      }
+
+      const result = await brandModel.updateOne({ _id: data.id }, data, {
+        new: true,
+      });
+      if (!result) {
+        throw new Error("brand not found.");
+      }
+      return result;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
+  async deleteBrandFromDB(id: string) {
+    try {
+      // Step 1: Check if the brand exists in the database
+      const isExist = await brandModel.findOne({ _id: id });
+
+      if (!isExist) {
+        throw new AppError(status.NOT_FOUND, "brand not found");
+      }
+
+      // Step 4: Delete the home brand from the database
+      await brandModel.updateOne({ _id: id }, { isDelete: true });
+      return;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`${error.message}`);
+      } else {
+        throw new Error("An unknown error occurred while fetching by ID.");
+      }
+    }
+  },
+};
