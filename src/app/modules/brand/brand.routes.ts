8309827f76f2be9d947660f21ad67491f6ad0d28
safimiazi <mohibullamiazi@gmail.com>
2025-04-20 10:03:02 +0600

    feat: add .npmignore and .travis.yml files for various modules, implement basic module exports, and update package.json dependencies

--- src/app/modules/brand/brand.routes.ts
+++ src/app/modules/brand/brand.routes.ts
@@ -4,14 +4,29 @@ import { brandController } from "./brand.controller";
 import { brandPostValidation, brandUpdateValidation } from "./brand.validation";
 import { authenticate, authorize } from "../../middlewares/authGuard";
 import { ROLE } from "../../constant/role";
+import { photoComposure } from "../../middlewares/photoComposure";
+import { processBrandImage } from "../../middlewares/fileProcessor/processBrandImage";
+import { getMuler } from "../../middlewares/multer";
 
 const router = express.Router();
+const { configurableCompression } = photoComposure();
 
 router.post(
   "/post_brand",
-  validateRequest(brandPostValidation),
   authenticate,
   authorize(ROLE.ADMIN, ROLE.VENDOR),
+  getMuler({
+    upload_file_destination_path: "uploads",
+    regex: /\.(jpg|jpeg|png|webp)$/,
+    images: "jpg, jpeg, png, webp",
+  }).fields([
+    { name: "brandImage", maxCount: 1 }, 
+  ]),
+
+  configurableCompression("jpeg", 60),
+  processBrandImage,
+  validateRequest(brandPostValidation),
+
   brandController.postBrand
 );
 router.get("/get_all_brand", brandController.getAllBrand);
