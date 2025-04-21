76e282be3dac24227e81218c98efff66d9b5d9d8
abdul <alimsujon12@gmail.com>
2025-04-21 12:46:15 +0600

    update vendors.routes.ts

--- src/app/modules/vendors/vendors.routes.ts
+++ src/app/modules/vendors/vendors.routes.ts
@@ -1,15 +1,25 @@
+import express from "express";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { vendorsController } from "./vendors.controller";
+import {
+  vendorsPostValidation,
+  vendorsUpdateValidation,
+} from "./vendors.validation";
 
-    import express from "express";
-    import { validateRequest } from "../../middlewares/validateRequest";
-    import { vendorsController } from "./vendors.controller";
-    import { vendorsPostValidation,vendorsUpdateValidation } from "./vendors.validation";
+const router = express.Router();
 
-    const router = express.Router();
-    
-    router.post("/post_vendors", validateRequest(vendorsPostValidation), vendorsController.postVendors);
-    router.get("/get_all_vendors", vendorsController.getAllVendors);
-    router.get("/get_single_vendors/:id", vendorsController.getSingleVendors);
-    router.put("/update_vendors/:id", validateRequest(vendorsUpdateValidation), vendorsController.updateVendors);
-    router.delete("/delete_vendors/:id", vendorsController.deleteVendors);
-    
-    export const vendorsRoutes = router;
+router.post(
+  "/post_vendors",
+  validateRequest(vendorsPostValidation),
+  vendorsController.postVendors
+);
+router.get("/get_all_vendors", vendorsController.getAllVendors);
+router.get("/get_single_vendors/:id", vendorsController.getSingleVendors);
+router.put(
+  "/update_vendors/:id",
+  validateRequest(vendorsUpdateValidation),
+  vendorsController.updateVendors
+);
+router.delete("/delete_vendors/:id", vendorsController.deleteVendors);
+
+export const vendorsRoutes = router;
