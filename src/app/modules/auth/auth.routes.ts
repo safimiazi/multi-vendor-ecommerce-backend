6183bf7bcb284c980a306d74518640091e05fb76
abdul <alimsujon12@gmail.com>
2025-04-21 12:45:41 +0600

    update 3 files and create 1 file

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -3,8 +3,6 @@ import { validateRequest } from "../../middlewares/validateRequest";
 import { authController } from "./auth.controller";
 import { authLoginValidation, authRegisterValidation } from "./auth.validation";
 import hashPassword from "../../middlewares/hashPassword";
-import { authenticate, authorize } from "../../middlewares/authGuard";
-import { ROLE } from "../../constant/role";
 
 const router = express.Router();
 
